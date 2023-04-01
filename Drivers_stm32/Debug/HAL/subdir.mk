f1bd5ceec19bf087cffcb1673876a433ec3048ca
belaleltaiby <83679802+belaleltaiby@users.noreply.github.com>
2023-04-01 16:45:32 +0200

    Add files via upload

---
+++ Drivers_stm32/Debug/HAL/subdir.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../HAL/KeyPad.c \
+../HAL/LCD.c 
+
+OBJS += \
+./HAL/KeyPad.o \
+./HAL/LCD.o 
+
+C_DEPS += \
+./HAL/KeyPad.d \
+./HAL/LCD.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+HAL/KeyPad.o: ../HAL/KeyPad.c
+	arm-none-eabi-gcc -gdwarf-2 "$<" -mcpu=cortex-m3 -std=gnu11 -g3 -DSTM32 -DSTM32F1 -DSTM32F103C6Tx -DDEBUG -c -I../Inc -I"G:/STMWORKSPACE/Drivers/HAL/Includes" -I"G:/STMWORKSPACE/Drivers/stm32_f103c6_drivers/Inc" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"HAL/KeyPad.d" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+HAL/LCD.o: ../HAL/LCD.c
+	arm-none-eabi-gcc -gdwarf-2 "$<" -mcpu=cortex-m3 -std=gnu11 -g3 -DSTM32 -DSTM32F1 -DSTM32F103C6Tx -DDEBUG -c -I../Inc -I"G:/STMWORKSPACE/Drivers/HAL/Includes" -I"G:/STMWORKSPACE/Drivers/stm32_f103c6_drivers/Inc" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"HAL/LCD.d" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+
---
+++ Drivers_stm32/Debug/stm32_f103c6_drivers/subdir.mk
@@ -0,0 +1,29 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../stm32_f103c6_drivers/stm32_f103c6_drivers_EXTI.c \
+../stm32_f103c6_drivers/stm32_f103c6_drivers_gpio.c \
+../stm32_f103c6_drivers/stm32_f103c6_drivers_timers.c 
+
+OBJS += \
+./stm32_f103c6_drivers/stm32_f103c6_drivers_EXTI.o \
+./stm32_f103c6_drivers/stm32_f103c6_drivers_gpio.o \
+./stm32_f103c6_drivers/stm32_f103c6_drivers_timers.o 
+
+C_DEPS += \
+./stm32_f103c6_drivers/stm32_f103c6_drivers_EXTI.d \
+./stm32_f103c6_drivers/stm32_f103c6_drivers_gpio.d \
+./stm32_f103c6_drivers/stm32_f103c6_drivers_timers.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+stm32_f103c6_drivers/stm32_f103c6_drivers_EXTI.o: ../stm32_f103c6_drivers/stm32_f103c6_drivers_EXTI.c
+	arm-none-eabi-gcc -gdwarf-2 "$<" -mcpu=cortex-m3 -std=gnu11 -g3 -DSTM32 -DSTM32F1 -DSTM32F103C6Tx -DDEBUG -c -I../Inc -I"G:/STMWORKSPACE/Drivers/HAL/Includes" -I"G:/STMWORKSPACE/Drivers/stm32_f103c6_drivers/Inc" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"stm32_f103c6_drivers/stm32_f103c6_drivers_EXTI.d" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+stm32_f103c6_drivers/stm32_f103c6_drivers_gpio.o: ../stm32_f103c6_drivers/stm32_f103c6_drivers_gpio.c
+	arm-none-eabi-gcc -gdwarf-2 "$<" -mcpu=cortex-m3 -std=gnu11 -g3 -DSTM32 -DSTM32F1 -DSTM32F103C6Tx -DDEBUG -c -I../Inc -I"G:/STMWORKSPACE/Drivers/HAL/Includes" -I"G:/STMWORKSPACE/Drivers/stm32_f103c6_drivers/Inc" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"stm32_f103c6_drivers/stm32_f103c6_drivers_gpio.d" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+stm32_f103c6_drivers/stm32_f103c6_drivers_timers.o: ../stm32_f103c6_drivers/stm32_f103c6_drivers_timers.c
+	arm-none-eabi-gcc -gdwarf-2 "$<" -mcpu=cortex-m3 -std=gnu11 -g3 -DSTM32 -DSTM32F1 -DSTM32F103C6Tx -DDEBUG -c -I../Inc -I"G:/STMWORKSPACE/Drivers/HAL/Includes" -I"G:/STMWORKSPACE/Drivers/stm32_f103c6_drivers/Inc" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"stm32_f103c6_drivers/stm32_f103c6_drivers_timers.d" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+
